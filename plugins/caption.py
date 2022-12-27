6b5204e6c70ff51e4201ea9c03cdb178dd9d976e
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2022-12-27 23:39:24 +0000

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -2,28 +2,28 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helper.database import *
 
-@Client.on_message(filters.private & filters.command('isim_ekle'))
+@Client.on_message(filters.private & filters.command('aciklama_ekle'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Ayarlamam için bana bir başlık ver.\n\nÖrnek:- `/dosya_ismi DW Store`**")
+       return await message.reply_text("**Ayarlamam için bana bir açıklama ver.\n\nÖrnek:- `/dosya_ismi DW Store`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
-    await message.reply_text("**Dosya ismi başarıyla düzenlendi ✅**")
+    await message.reply_text("**Dosya açıklaması başarıyla düzenlendi ✅**")
 
-@Client.on_message(filters.private & filters.command('isim_sil'))
+@Client.on_message(filters.private & filters.command('aciklama_sil'))
 async def delete_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if not caption:
-        await message.reply_text("**Herhangi bir özel başlığınız yok**")
+        await message.reply_text("**Herhangi bir özel açıklama yok**")
         return
     delcaption(int(message.chat.id))
-    await message.reply_text("**Dosya ismi başarı ile silindi ✅**")
+    await message.reply_text("**Dosya açıklaması başarı ile silindi ✅**")
                                        
-@Client.on_message(filters.private & filters.command('ismi_gör'))
+@Client.on_message(filters.private & filters.command('aciklamayi_gör'))
 async def see_caption(client, message): 
     caption = find(int(message.chat.id))[1]
     if caption:
-       await message.reply_text(f"<b><u>Eklenen isim:</b></u>\n\n`{caption}`")
+       await message.reply_text(f"<b><u>Eklenen açıklama:</b></u>\n\n`{caption}`")
     else:
-       await message.reply_text("**Herhangi bir özel başlığınız yok**")
+       await message.reply_text("**Herhangi bir özel açıklama yok**")
           
